577dc857191820f1efe49cf5778439bc9b150175
Stackbit Code Editor <projects@stackbit.com>
2022-06-16 21:36:51 +0000

    index.ts: changed

--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -4,6 +4,6 @@ import PostFeedCategoryLayout from './PostFeedCategoryLayout';
 import PostLayout from './PostLayout';
 import BlankBaseLayout from './BlankBaseLayout';
 import DefaultBaseLayout from './DefaultBaseLayout';
-import DefaultBaseLayout from './DefaultBaseLayout';
+import ContactLayout from './ContactLayout';
 
 export { PageLayout, PostFeedLayout, PostFeedCategoryLayout, PostLayout, BlankBaseLayout, DefaultBaseLayout };
